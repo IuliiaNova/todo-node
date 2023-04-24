033527784f2af14d551b111596a604d0fe42497d
Iuliia Shikhanova <yshihanova@gmail.com>
2023-04-25 00:50:23 +0100

    create: jwt middleware

---
+++ middleware/VerifyToken.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+export const VerifyJwtToken = async(req,res,next) => {
+  try{
+    const token = req.cookies.token.token
+    if(!token) return res.status(401).json({mesage: 'Not authorized'})
+    jwt.verify(token , process.env.SECRET_KEY , (err, user) => {
+      if(err) {
+          return res.status(401).json({message: 'Wrong credentials'})
+      }
+      // return res.json({user})
+      req.user = user
+    })
+    next()
+  }
+  catch(err) {
+      next(err)
+  }
+}
