0d79bbf26374bea6b21548761e01932bdfe20417
Iuliia Shikhanova <yshihanova@gmail.com>
2023-04-25 01:35:53 +0100

    create: task control. and push more data for check

---
+++ data/tasks.js
@@ -0,0 +1,25 @@
+const tasks = [
+  {
+      "userId": "63c26f9ced9c25f363709564",
+      "type": "work",
+      "name": "Meeting",
+      "date": "2023-12-01",
+      "time": "06:00",
+  },
+  {
+      "userId": "63c26f9ced9c25f363709564",
+      "type": "sport",
+      "name": "Gym",
+      "date": "2023-03-02",
+      "time": "06:00",
+  },
+  {
+      "userId": "63c26f9ced9c25f363709564",
+      "type": "shopping",
+      "name": "Mercadona",
+      "date": "2023-4-05",
+      "time": "08:00",
+  },
+]
+
+export default tasks
